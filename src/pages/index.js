013823865a1e840c7ead9589c52cca0a1987eba0
esser-divad <dcr54@uakron.edu>
2024-02-21 03:02:32 -0500

    Reconnect stylesheets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,32 @@
 // src/pages/index.js
 
 import Head from 'next/head';
-import styles from '../styles/globals.css'; // Adjust the path according to your styles location
+import global_styles from '../styles/globals.css';
+import index_styles from '../styles/index.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={index_styles.container}>
       <Head>
         <title>🔨 David Reese's Portfolio 📐</title>
         <meta name="description" content="Homepage for David Reese's Portfolio & Development Projects." />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className={index_styles.main}>
+        <h1 className={index_styles.title}>
           Welcome to My Next.js Homepage!
         </h1>
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>src/pages/index.js</code>
+        <p className={index_styles.description}>
+          Get started by editing <code className={index_styles.code}>src/pages/index.js</code>
         </p>
 
         {/* Add more content here as needed */}
       </main>
 
-      <footer className={styles.footer}>
-        {/* Footer content here */}
+      <footer className={index_styles.footer}>
+        <p className={index_styles.description}>
+            This is a (React/Next/Bootstrap 5) portfolio website hosting the projects of David Reese. 
+        </p>
       </footer>
     </div>
   );
