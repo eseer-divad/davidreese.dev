65aa6e720d4743af49c1f5050a52a465973b18cd
esser-divad <dcr54@uakron.edu>
2024-02-26 02:48:21 -0500

    Add projects section & format w/ bootstrap grids

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import RootLayout from '@/app/layout';
 import Head from 'next/head';
 import global_styles from '../styles/globals.css';
 import index_styles from '../styles/index.module.css';
+import { Card } from 'react-bootstrap';
 
 export default function Home() {
   // Cycle through title array, keep state
   // TODO: "a developer" vs "a engineer" grammar
-  const titles = ['developer.', 'designer.', 'engineer.', 'sysadmin.', ]
+  const titles = ['a developer.', 'a designer.', 'an engineer.', 'a SysAdmin.', ]
   const [currentTitle, setCurrentTitle] = useState(titles[0]);
 
   useEffect(() => {
@@ -28,17 +29,36 @@ export default function Home() {
       <Head>
         <title>Development Portfolio & Blog</title>
         <meta name="description" content="Homepage for David Reese's Portfolio & Development Projects." />
-      </Head> 
-
+      </Head>
       <main className={index_styles.main}>
-        <h1 className={index_styles.title}>
-        David is a {currentTitle}
-        </h1>
-        <ul className={index_styles.bulletPoints}>
-          <li>Currently a Web Designer at the Buchtel College of Arts & Sciences.</li>
-          <li>Previously a Support Analyst, then Systems Administrator Intern with the IT Department.</li>
-          <li>Employed & earning a B.S. of Computer Science @ The University of Akron.</li>
-        </ul>
+        <div class="container">
+          <div class="row">
+            <h1 className={index_styles.title}>
+                  David is {currentTitle}
+            </h1>
+            <div class="row">
+              <ul className={index_styles.bulletPoints}>
+                <p>Currently a Web Designer at the Buchtel College of Arts & Sciences.</p>
+                <p>Previously a Support Analyst, then Systems Administrator Intern with the IT Department.</p>
+                <p>Employed & earning a B.S. of Computer Science @ The University of Akron.</p>
+              </ul>
+            </div>
+          </div>
+          <h2 className={index_styles.title}>
+            Projects:
+          </h2>
+          <div class="row">
+            <Card class="col-sm">
+              <p>aaaa</p>
+            </Card>
+            <Card class="col-sm">
+              <p>aaaa</p>
+            </Card>
+            <Card class="col-sm">
+              <p>aaaa</p>
+            </Card>
+          </div>
+        </div>
       </main>   
     </div>
   );
